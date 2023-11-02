ClaytonNorthey92/btcd

Added test for client.GetChainTxStatsAsync() in rpcclient.  This sets up a test websocket server to run the tests.  Also, ensure these are run within a timeout, since they rely on concurrency

rpcclient/chain_test.go
@@ -1,6 +1,15 @@
 package rpcclient
 
-import "testing"
+import (
+	"github.com/gorilla/websocket"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+)
+
+var upgrader = websocket.Upgrader{}
 
 // TestUnmarshalGetBlockChainInfoResult ensures that the SoftForks and
 // UnifiedSoftForks fields of GetBlockChainInfoResult are properly unmarshaled
@@ -87,6 +96,125 @@ func TestUnmarshalGetBlockChainInfoResultSoftForks(t *testing.T) {
 		})
 		if !success {
 			return
+		}
+	}
+}
+
+func TestClientConnectedToWSServerRunner(t *testing.T) {
+	type TestTableItem struct {
+		Name     string
+		TestCase func(t *testing.T)
+	}
+
+	testTable := []TestTableItem{
+		TestTableItem{
+			Name: "TestGetChainTxStatsAsyncSuccessTx",
+			TestCase: func(t *testing.T) {
+				client, serverReceivedChannel, cleanup := makeClient(t)
+				defer cleanup()
+				client.GetChainTxStatsAsync()
+
+				message := <-serverReceivedChannel
+				if message != "{\"jsonrpc\":\"1.0\",\"method\":\"getchaintxstats\",\"params\":[],\"id\":1}" {
+					t.Fatalf("received unexpected message: %s", message)
+				}
+			},
+		},
+		TestTableItem{
+			Name: "TestGetChainTxStatsAsyncShutdownError",
+			TestCase: func(t *testing.T) {
+				client, _, cleanup := makeClient(t)
+				defer cleanup()
+
+				// a bit of a hack here: since there are multiple places where we read
+				// from the shutdown channel, and it is not buffered, ensure that a shutdown
+				// message is sent every time it is read from, this will ensure that
+				// when client.GetChainTxStatsAsync() gets called, it hits the non-blocking
+				// read from the shutdown channel
+				go func() {
+					type shutdownMessage struct{}
+					for {
+						client.shutdown <- shutdownMessage{}
+					}
+				}()
+
+				var response *Response = nil
+
+				for response == nil {
+					respChan := client.GetChainTxStatsAsync()
+					select {
+					case response = <-respChan:
+					default:
+					}
+				}
+
+				if response.err == nil || response.err.Error() != "the client has been shutdown" {
+					t.Fatalf("unexpected error: %s", response.err.Error())
+				}
+			},
+		},
+	}
+
+	// since these tests rely on concurrency, ensure there is a resonable timeout
+	// that they should run within
+	for _, testCase := range testTable {
+		done := make(chan bool)
+
+		go func() {
+			t.Run(testCase.Name, testCase.TestCase)
+			done <- true
+		}()
+
+		select {
+		case <-done:
+		case <-time.After(5 * time.Second):
+			t.Fatalf("timeout exceeded for: %s", testCase.Name)
+		}
+	}
+}
+
+func makeClient(t *testing.T) (*Client, chan string, func()) {
+	serverReceivedChannel := make(chan string)
+	s := httptest.NewServer(http.HandlerFunc(makeUpgradeOneConnect(serverReceivedChannel)))
+	url := strings.TrimPrefix(s.URL, "http://")
+
+	config := ConnConfig{
+		DisableTLS: true,
+		User:       "username",
+		Pass:       "password",
+		Host:       url,
+	}
+
+	client, err := New(&config, nil)
+	if err != nil {
+		t.Fatalf("error when creating new client %s", err.Error())
+	}
+	return client, serverReceivedChannel, func() {
+		s.Close()
+	}
+}
+
+func makeUpgradeOneConnect(ch chan string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			return
+		}
+		defer c.Close()
+		for {
+			mt, message, err := c.ReadMessage()
+			if err != nil {
+				break
+			}
+
+			go func() {
+				ch <- string(message)
+			}()
+
+			err = c.WriteMessage(mt, []byte("blahhhhhh"))
+			if err != nil {
+				break
+			}
 		}
 	}
 }

organicbitcoin/obtcd

rpcclient/chain_test.go
@@ -2,8 +2,15 @@ package rpcclient
 
 import (
 	"errors"
+	"github.com/gorilla/websocket"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
+	"time"
 )
+
+var upgrader = websocket.Upgrader{}
 
 // TestUnmarshalGetBlockChainInfoResult ensures that the SoftForks and
 // UnifiedSoftForks fields of GetBlockChainInfoResult are properly unmarshaled
@@ -127,5 +134,119 @@ func TestFutureGetBlockCountResultReceiveMarshalsResponseCorrectly(t *testing.T)
 
 	if res != 66 {
 		t.Fatalf("unexpected response: %d (0x%X)", res, res)
+	}
+}
+
+func TestClientConnectedToWSServerRunner(t *testing.T) {
+	type TestTableItem struct {
+		Name     string
+		TestCase func(t *testing.T)
+	}
+
+	testTable := []TestTableItem{
+		TestTableItem{
+			Name: "TestGetChainTxStatsAsyncSuccessTx",
+			TestCase: func(t *testing.T) {
+				client, serverReceivedChannel, cleanup := makeClient(t)
+				defer cleanup()
+				client.GetChainTxStatsAsync()
+
+				message := <-serverReceivedChannel
+				if message != "{\"jsonrpc\":\"1.0\",\"method\":\"getchaintxstats\",\"params\":[],\"id\":1}" {
+					t.Fatalf("received unexpected message: %s", message)
+				}
+			},
+		},
+		TestTableItem{
+			Name: "TestGetChainTxStatsAsyncShutdownError",
+			TestCase: func(t *testing.T) {
+				client, _, cleanup := makeClient(t)
+				defer cleanup()
+
+				// a bit of a hack here: since there are multiple places where we read
+				// from the shutdown channel, and it is not buffered, ensure that a shutdown
+				// message is sent every time it is read from, this will ensure that
+				// when client.GetChainTxStatsAsync() gets called, it hits the non-blocking
+				// read from the shutdown channel
+				go func() {
+					type shutdownMessage struct{}
+					for {
+						client.shutdown <- shutdownMessage{}
+					}
+				}()
+
+				var response *Response = nil
+
+				for response == nil {
+					respChan := client.GetChainTxStatsAsync()
+					select {
+					case response = <-respChan:
+					default:
+					}
+				}
+
+				if response.err == nil || response.err.Error() != "the client has been shutdown" {
+					t.Fatalf("unexpected error: %s", response.err.Error())
+				}
+			},
+		},
+	}
+
+	// since these tests rely on concurrency, ensure there is a resonable timeout
+	// that they should run within
+	for _, testCase := range testTable {
+		done := make(chan bool)
+
+		go func() {
+			t.Run(testCase.Name, testCase.TestCase)
+			done <- true
+		}()
+
+		select {
+		case <-done:
+		case <-time.After(5 * time.Second):
+			t.Fatalf("timeout exceeded for: %s", testCase.Name)
+		}
+	}
+}
+
+func makeClient(t *testing.T) (*Client, chan string, func()) {
+	serverReceivedChannel := make(chan string)
+	s := httptest.NewServer(http.HandlerFunc(makeUpgradeOnConnect(serverReceivedChannel)))
+	url := strings.TrimPrefix(s.URL, "http://")
+
+	config := ConnConfig{
+		DisableTLS: true,
+		User:       "username",
+		Pass:       "password",
+		Host:       url,
+	}
+
+	client, err := New(&config, nil)
+	if err != nil {
+		t.Fatalf("error when creating new client %s", err.Error())
+	}
+	return client, serverReceivedChannel, func() {
+		s.Close()
+	}
+}
+
+func makeUpgradeOnConnect(ch chan string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			return
+		}
+		defer c.Close()
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				break
+			}
+
+			go func() {
+				ch <- string(message)
+			}()
+		}
 	}
 }