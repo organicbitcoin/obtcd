btcsuite/btcd

wire: add TestNet4

wire/protocol.go
@@ -179,6 +179,9 @@ const (
 	// TestNet3 represents the test network (version 3).
 	TestNet3 BitcoinNet = 0x0709110b
 
+	// TestNet4 represents the test network (version 4).
+	TestNet4 BitcoinNet = 0x1c163f28
+
 	// SigNet represents the public default SigNet. For custom signets,
 	// see CustomSignetParams.
 	SigNet BitcoinNet = 0x40CF030A
@@ -193,6 +196,7 @@ var bnStrings = map[BitcoinNet]string{
 	MainNet:  "MainNet",
 	TestNet:  "TestNet",
 	TestNet3: "TestNet3",
+	TestNet4: "TestNet4",
 	SigNet:   "SigNet",
 	SimNet:   "SimNet",
 }

organicbitcoin/obtcd

wire/protocol.go
@@ -179,6 +179,9 @@ const (
 	// TestNet3 represents the test network (version 3).
 	TestNet3 BitcoinNet = 0x0709110b
 
+	// TestNet4 represents the test network (version 4).
+	TestNet4 BitcoinNet = 0x283f161c
+
 	// SigNet represents the public default SigNet. For custom signets,
 	// see CustomSignetParams.
 	SigNet BitcoinNet = 0x40CF030A
@@ -193,6 +196,7 @@ var bnStrings = map[BitcoinNet]string{
 	MainNet:  "MainNet",
 	TestNet:  "TestNet",
 	TestNet3: "TestNet3",
+	TestNet4: "TestNet4",
 	SigNet:   "SigNet",
 	SimNet:   "SimNet",
 }