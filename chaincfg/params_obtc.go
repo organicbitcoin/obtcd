806437fedba6695411fe1aaf659e6360970877d7
OBTC Maintainer <maintainer@organicbitcoin.org>
2025-10-12 22:31:22 +0200

    fix: add missing Bech32HRPSegwit for ObtcRegTestParams
    The ObtcRegTestParams was missing the Bech32HRPSegwit field, which caused
    it to use an empty string. When the Register function processed this
    parameter, it added '1' (empty string + '1') to the bech32SegwitPrefixes
    map, making all bech32 prefixes ending with '1' valid and breaking the
    TestRegister test.
    
    Changes:
    - Add Bech32HRPSegwit: 'obtcrt' for regtest network
    - Add complete address encoding parameters for regtest
    - Change testnet Bech32HRPSegwit from 'tob' to 'obtct' for clarity
    
    All tests now pass including the previously failing TestRegister.

--- chaincfg/params_obtc.go
+++ chaincfg/params_obtc.go
@@ -16,7 +16,7 @@ import (
 var obtcPowLimit = new(big.Int).Sub(new(big.Int).Lsh(bigOne, 224), bigOne)
 
 // ObtcMainNetParams defines the network parameters for the OBTC main network.
-// 
+//
 // Note: This is currently a skeleton implementation for Week 1 of the OBTC
 // development plan. Many parameters are placeholders and will be finalized
 // in Week 3 when the genesis block and unique constants are frozen.
@@ -44,7 +44,7 @@ var ObtcMainNetParams = Params{
 	BIP0065Height:            1,    // Enable CHECKLOCKTIMEVERIFY from block 1
 	BIP0066Height:            1,    // Enable strict DER signatures from block 1
 	CoinbaseMaturity:         100,
-	SubsidyReductionInterval: 210000, // Same as Bitcoin for now
+	SubsidyReductionInterval: 210000,              // Same as Bitcoin for now
 	TargetTimespan:           time.Hour * 24 * 14, // 14 days
 	TargetTimePerBlock:       time.Minute * 10,    // 10 minutes
 	RetargetAdjustmentFactor: 4,                   // 25% less, 400% more
@@ -60,28 +60,28 @@ var ObtcMainNetParams = Params{
 	MinerConfirmationWindow:       2016, // 2 weeks worth of blocks
 	Deployments: [DefinedDeployments]ConsensusDeployment{
 		DeploymentTestDummy: {
-			BitNumber:           28,
-			AlwaysActiveHeight:  0, // Always active for OBTC
-			DeploymentStarter:   NewMedianTimeDeploymentStarter(time.Unix(0, 0)),
-			DeploymentEnder:     NewMedianTimeDeploymentEnder(time.Unix(0, 0)),
+			BitNumber:          28,
+			AlwaysActiveHeight: 0, // Always active for OBTC
+			DeploymentStarter:  NewMedianTimeDeploymentStarter(time.Unix(0, 0)),
+			DeploymentEnder:    NewMedianTimeDeploymentEnder(time.Unix(0, 0)),
 		},
 		DeploymentCSV: {
-			BitNumber:           0,
-			AlwaysActiveHeight:  1, // Active from block 1
-			DeploymentStarter:   NewMedianTimeDeploymentStarter(time.Unix(0, 0)),
-			DeploymentEnder:     NewMedianTimeDeploymentEnder(time.Unix(0, 0)),
+			BitNumber:          0,
+			AlwaysActiveHeight: 1, // Active from block 1
+			DeploymentStarter:  NewMedianTimeDeploymentStarter(time.Unix(0, 0)),
+			DeploymentEnder:    NewMedianTimeDeploymentEnder(time.Unix(0, 0)),
 		},
 		DeploymentSegwit: {
-			BitNumber:           1,
-			AlwaysActiveHeight:  1, // Active from block 1
-			DeploymentStarter:   NewMedianTimeDeploymentStarter(time.Unix(0, 0)),
-			DeploymentEnder:     NewMedianTimeDeploymentEnder(time.Unix(0, 0)),
+			BitNumber:          1,
+			AlwaysActiveHeight: 1, // Active from block 1
+			DeploymentStarter:  NewMedianTimeDeploymentStarter(time.Unix(0, 0)),
+			DeploymentEnder:    NewMedianTimeDeploymentEnder(time.Unix(0, 0)),
 		},
 		DeploymentTaproot: {
-			BitNumber:           2,
-			AlwaysActiveHeight:  1, // Active from block 1
-			DeploymentStarter:   NewMedianTimeDeploymentStarter(time.Unix(0, 0)),
-			DeploymentEnder:     NewMedianTimeDeploymentEnder(time.Unix(0, 0)),
+			BitNumber:          2,
+			AlwaysActiveHeight: 1, // Active from block 1
+			DeploymentStarter:  NewMedianTimeDeploymentStarter(time.Unix(0, 0)),
+			DeploymentEnder:    NewMedianTimeDeploymentEnder(time.Unix(0, 0)),
 		},
 	},
 
@@ -93,7 +93,7 @@ var ObtcMainNetParams = Params{
 	// address reuse and replay attacks between Bitcoin and OBTC networks
 	Bech32HRPSegwit:         "obtc", // Human-readable part for bech32 addresses
 	PubKeyHashAddrID:        0x47,   // TODO: Generate unique value (currently placeholder)
-	ScriptHashAddrID:        0x32,   // TODO: Generate unique value (currently placeholder)  
+	ScriptHashAddrID:        0x32,   // TODO: Generate unique value (currently placeholder)
 	PrivateKeyID:            0xEF,   // TODO: Generate unique value (currently placeholder)
 	WitnessPubKeyHashAddrID: 0x06,   // TODO: Generate unique value (currently placeholder)
 	WitnessScriptHashAddrID: 0x0A,   // TODO: Generate unique value (currently placeholder)
@@ -112,20 +112,20 @@ var ObtcMainNetParams = Params{
 var ObtcTestNetParams = Params{
 	Name:        "obtctestnet",
 	Net:         wire.ObtcTestNet, // Use separate magic for testnet
-	DefaultPort: "18555",         // Different from Bitcoin testnet's 18333
+	DefaultPort: "18555",          // Different from Bitcoin testnet's 18333
 
 	// Most parameters inherit from mainnet but with easier difficulty
-	PowLimit:                 obtcPowLimit,
-	PowLimitBits:             0x1d00ffff,
-	PoWNoRetargeting:         false,
-	ReduceMinDifficulty:      true,                  // Allow difficulty reduction
-	MinDiffReductionTime:     time.Minute * 20,      // Reduce difficulty after 20 min
-	GenerateSupported:        true,                  // Allow CPU mining
-	RuleChangeActivationThreshold: 1512,            // 75% instead of 95%
+	PowLimit:                      obtcPowLimit,
+	PowLimitBits:                  0x1d00ffff,
+	PoWNoRetargeting:              false,
+	ReduceMinDifficulty:           true,             // Allow difficulty reduction
+	MinDiffReductionTime:          time.Minute * 20, // Reduce difficulty after 20 min
+	GenerateSupported:             true,             // Allow CPU mining
+	RuleChangeActivationThreshold: 1512,             // 75% instead of 95%
 	MinerConfirmationWindow:       2016,
-	
+
 	// Same address encoding as mainnet - they'll be distinguished by network context
-	Bech32HRPSegwit:         "tob", // "test obtc" 
+	Bech32HRPSegwit:         "obtct", // "obtc test"
 	PubKeyHashAddrID:        0x6F,  // Different from mainnet
 	ScriptHashAddrID:        0xC4,  // Different from mainnet
 	PrivateKeyID:            0xEF,
@@ -133,31 +133,31 @@ var ObtcTestNetParams = Params{
 	WitnessScriptHashAddrID: 0x28,
 	HDPrivateKeyID:          [4]byte{0x04, 0x35, 0x83, 0x94}, // "tprv" equivalent
 	HDPublicKeyID:           [4]byte{0x04, 0x35, 0x87, 0xCF}, // "tpub" equivalent
-	HDCoinType:              1, // Testnet coin type
-	
+	HDCoinType:              1,                               // Testnet coin type
+
 	// TODO: Complete remaining fields in Week 3
 }
 
 // ObtcRegTestParams defines the network parameters for OBTC regression testing.
 var ObtcRegTestParams = Params{
-	Name:        "obtcregtest", 
+	Name:        "obtcregtest",
 	Net:         wire.ObtcRegNet, // Use separate magic for regtest
 	DefaultPort: "18444",         // Same as Bitcoin regtest for now
 
 	// Regression test parameters - very easy mining
-	PowLimit:                regressionPowLimit,
-	PowLimitBits:            0x207fffff,
-	PoWNoRetargeting:        true,
-	ReduceMinDifficulty:     false,
-	GenerateSupported:       true,
-	CoinbaseMaturity:        100,
-	BIP0034Height:           100000000, // Not activated on regtest
-	BIP0065Height:           1351,
-	BIP0066Height:           1251,
+	PowLimit:                 regressionPowLimit,
+	PowLimitBits:             0x207fffff,
+	PoWNoRetargeting:         true,
+	ReduceMinDifficulty:      false,
+	GenerateSupported:        true,
+	CoinbaseMaturity:         100,
+	BIP0034Height:            100000000, // Not activated on regtest
+	BIP0065Height:            1351,
+	BIP0066Height:            1251,
 	SubsidyReductionInterval: 150,
-	TargetTimespan:          time.Hour * 24 * 14, // 14 days
-	TargetTimePerBlock:      time.Minute * 10,    // 10 minutes  
-	RetargetAdjustmentFactor: 4,                  // 25% less, 400% more
+	TargetTimespan:           time.Hour * 24 * 14, // 14 days
+	TargetTimePerBlock:       time.Minute * 10,    // 10 minutes
+	RetargetAdjustmentFactor: 4,                   // 25% less, 400% more
 
 	// No checkpoints for regtest
 	Checkpoints: []Checkpoint{},
@@ -166,15 +166,26 @@ var ObtcRegTestParams = Params{
 	RuleChangeActivationThreshold: 108, // 75% of MinerConfirmationWindow
 	MinerConfirmationWindow:       144, // Faster than normal
 	
+	// Address encoding for regtest
+	Bech32HRPSegwit:         "obtcrt", // "obtc regtest"
+	PubKeyHashAddrID:        0x6F,     // Same as Bitcoin regtest
+	ScriptHashAddrID:        0xC4,     // Same as Bitcoin regtest  
+	PrivateKeyID:            0xEF,     // Same as Bitcoin regtest
+	WitnessPubKeyHashAddrID: 0x03,     // Same as Bitcoin regtest
+	WitnessScriptHashAddrID: 0x28,     // Same as Bitcoin regtest
+	HDPrivateKeyID:          [4]byte{0x04, 0x35, 0x83, 0x94}, // Same as Bitcoin regtest
+	HDPublicKeyID:           [4]byte{0x04, 0x35, 0x87, 0xCF}, // Same as Bitcoin regtest
+	HDCoinType:              1,        // Regtest coin type
+
 	// TODO: Complete remaining fields in Week 3
 }
 
 // IsOBTC returns true if the network parameters represent an OBTC network.
 // This function is critical for isolating OBTC-specific behavior from Bitcoin logic.
 func IsOBTC(params *Params) bool {
-	return params.Net == wire.ObtcMainNet || 
-		   params.Net == wire.ObtcTestNet || 
-		   params.Net == wire.ObtcRegNet
+	return params.Net == wire.ObtcMainNet ||
+		params.Net == wire.ObtcTestNet ||
+		params.Net == wire.ObtcRegNet
 }
 
 // init registers the OBTC network parameters so they can be used with the
@@ -185,12 +196,12 @@ func init() {
 	if err != nil {
 		panic("failed to register OBTC mainnet parameters: " + err.Error())
 	}
-	
+
 	err = Register(&ObtcTestNetParams)
 	if err != nil {
 		panic("failed to register OBTC testnet parameters: " + err.Error())
 	}
-	
+
 	err = Register(&ObtcRegTestParams)
 	if err != nil {
 		panic("failed to register OBTC regtest parameters: " + err.Error())
